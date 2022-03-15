336f7cb8b382145a9d12fb4fa764f4038fcc489d
hbrownwhyte <56229223+hbrownwhyte@users.noreply.github.com>
2022-03-14 21:52:48 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // This is for Lab 06 in COP 3502C Spring 2022
+        // This lab is teaching us how to use Git and GitHub
+        //This program will first add a couple numbers, and after changes, subtract two numbers.
+
+
+        double numFirst = Math.random();
+        double numSecond = Math.random();
+
+        int total = (int) ((numFirst + numSecond) * 100);
+
+        System.out.println("The sum of two random numbers is " + (total) + "!");
+
+
+
+    }
+}
